e38586b5e14d4af72f52b2470fd8a77540d6a7c2
SongJinwoo1 <songjinwoo8491524@gmail.com>
2026-02-26 11:13:37 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// إعدادات Firebase العالمية لنظام Zenith OS
+// تم تجميع هذه البيانات بناءً على ملف Login.html الخاص بك
+export const firebaseConfig = {
+    apiKey: "AIzaSyC-Vfw10y-xcUL-U-DGhDgoi4oUjlNbpXg",
+    authDomain: "zenith-c26ae.firebaseapp.com",
+    projectId: "zenith-c26ae",
+    storageBucket: "zenith-c26ae.firebasestorage.app",
+    messagingSenderId: "146416524485",
+    appId: "1:146416524485:web:8aad935153f17f244924ac",
+    measurementId: "G-N61VN698E0"
+};
